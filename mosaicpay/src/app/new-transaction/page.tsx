b9e7585737af2547b87597876d66d1245f34c437
Haris Kordić <kordicharis18@gmail.com>
2023-07-09 16:42:14 +0200

    feat: new transaction initial UI render AB#68

---
+++ mosaicpay/src/app/new-transaction/page.tsx
@@ -0,0 +1,171 @@
+"use client";
+import React, { useState } from "react";
+import {
+	Alert,
+	Box,
+	Button,
+	FormControlLabel,
+	Grid,
+	InputLabel,
+	MenuItem,
+	Radio,
+	RadioGroup,
+	Select,
+	Typography,
+} from "@mui/material";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { getUserAccounts } from "../api";
+import { Cancel, Save } from "@mui/icons-material";
+
+export default function NewTransaction() {
+	const [showAlert, setShowAlert] = useState(false);
+	const [transactionState, setTransactionState] = useState(1);
+	const [accountState, setAccountState] = useState([]);
+	const [selectedAccount, setSelectedAccount] = useState("");
+
+	const [transactionType, setTransactionType] = useState("d");
+	const router = useRouter();
+
+	useEffect(() => {
+		const getData = async () => {
+			const data = await getUserAccounts();
+			const accounts = data.map((item: any) => ({
+				value: item.account_id,
+				name: item.name,
+			}));
+			setAccountState(accounts);
+			if (accounts.length > 0) {
+				setSelectedAccount(accounts[0].value);
+			}
+		};
+
+		getData();
+	}, []);
+
+	const handleSubmit = async () => {
+		// Handle form submission
+	};
+
+	const handleAccountChange = (event: any) => {
+		setSelectedAccount(event.target.value); 
+	};
+
+	return (
+		<Grid
+			display="flex"
+			flexDirection="column"
+			width="100%"
+			height="100%"
+			justifyContent="center"
+			padding={5} 
+		>
+			<Box>
+				<Typography variant="h4" component="h4" sx={{ textAlign: "center" }}>
+					Add transaction
+				</Typography>
+			</Box>
+			<Box width="100%" padding={2}>
+				<Box mt={3}>
+					<InputLabel id="transaction-state-simple-select-label">
+						Transaction type
+					</InputLabel>
+					<RadioGroup
+						aria-labelledby="transaction-type-radio-buttons-group-label"
+						defaultValue="d"
+						name="transaction-type-radio-buttons-group"
+						onChange={(event) => setTransactionType(event.target.value)}
+					>
+						<FormControlLabel
+							value="d"
+							control={<Radio color="secondary" />}
+							label="Debit"
+						/>
+						<FormControlLabel
+							value="c"
+							control={<Radio color="secondary" />}
+							label="Credit"
+							color="secondary"
+						/>
+					</RadioGroup>
+				</Box>
+				<Box mt={3}>
+					<InputLabel id="account-state-simple-select-label">
+						Account
+					</InputLabel>
+					<Select
+						fullWidth
+						labelId="account-state-select-label"
+						id="account-state-select"
+						name="account-state"
+						value={selectedAccount}
+						label="Account state"
+						onChange={handleAccountChange}
+						color="secondary"
+					>
+						{accountState.map((account: any) => (
+							<MenuItem value={account.value} key={account.value}>
+								{account.name}
+							</MenuItem>
+						))}
+					</Select>
+				</Box>
+				<Box mt={3}>
+					<InputLabel id="transaction-state-simple-select-label">
+						Transaction state
+					</InputLabel>
+					<Select
+						fullWidth
+						labelId="transaction-state-select-label"
+						id="transaction-state-select"
+						name="transaction-state"
+						value={transactionState}
+						label="Transaction state"
+						onChange={(e: any) => setTransactionState(e.target.value)}
+						color="secondary"
+					>
+						<MenuItem value={1}>Draft</MenuItem>
+						<MenuItem value={2}>Pending</MenuItem>
+						<MenuItem value={3}>Processed</MenuItem>
+						<MenuItem value={4}>Failed</MenuItem>
+					</Select>
+				</Box>
+
+				<Box display={"flex"} justifyContent={"space-between"} sx={{ mt: 10 }}>
+					<Button
+						startIcon={<Save></Save>}
+						color="secondary"
+						variant="outlined"
+						onClick={handleSubmit}
+					>
+						Save
+					</Button>
+					<Button
+						startIcon={<Cancel></Cancel>}
+						color="secondary"
+						variant="outlined"
+						onClick={() => router.push("/home")}
+					>
+						Cancel
+					</Button>
+				</Box>
+			</Box>
+			<Alert
+				sx={{ mt: 5, display: showAlert ? "flex" : "none" }}
+				action={
+					<Button
+						color="success"
+						size="small"
+						onClick={() => setShowAlert(false)}
+					>
+						X
+					</Button>
+				}
+				onClose={() => setShowAlert(false)}
+				severity="success"
+			>
+				Transaction successfully created!
+			</Alert>
+		</Grid>
+	);
+}
